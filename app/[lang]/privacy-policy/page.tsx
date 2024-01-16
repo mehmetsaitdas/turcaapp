610ed3b611f4fe1d680281637b37f920da65fc07
mehmetsaitdas <mehmetsaitdas@hotmail.com>
2024-01-16 23:04:11 +0300

    document_editinga

--- app/[lang]/privacy-policy/page.tsx
+++ app/[lang]/privacy-policy/page.tsx
@@ -1,6 +1,23 @@
-import Document from "../components/document";
-
+import { geMessages } from '@/lib/messages'
 import { Locale } from '@/i18n.config'
+
+type FeatureItem = {
+    title: string;
+    desc: string;
+}
+
+const FeatureItem = ({ title, desc }: FeatureItem) => {
+    return (
+        <li className="flex w-full flex-1 flex-col items-start">
+            <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
+                {title}
+            </h2>
+            <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+                {desc}
+            </p>
+        </li>
+    )
+}
 
 const PrivacyPolicy = async ({
     params: { lang },
@@ -8,12 +25,22 @@ const PrivacyPolicy = async ({
     params: { lang: Locale }
 }) => {
 
+    const { document } = await geMessages(lang)
+    const valuea = document.filter((i) => i.names == "privacyPolicy")[0];
+
     return (
         <>
-            <Document params={{ lang, names: "privacyPolicy" }} />
-            {/* <Guide params={{ lang, names }} />
-            <Features params={{ lang, names }} />
-            <GetApp params={{ lang, names }} /> */}
+            <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-10">
+                <div className="max-container padding-container relative w-full flex justify-end">
+                    <div className="z-20 flex w-full flex-col lg:w-[60%]">
+                        <ul className="mt-0 grid gap-10 md:grid-cols-1 lg:mg-20 lg:gap-20">
+                            <FeatureItem
+                                title={valuea.title}
+                                desc={valuea.detai} />
+                        </ul>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- app/[lang]/terms-of-use/page.tsx
+++ app/[lang]/terms-of-use/page.tsx
@@ -1,8 +1,23 @@
-import Guide from "../components/guide";
-import Features from "../components/features";
-import GetApp from "../components/get-app";
-
+import { geMessages } from '@/lib/messages'
 import { Locale } from '@/i18n.config'
+
+type FeatureItem = {
+    title: string;
+    desc: string;
+}
+
+const FeatureItem = ({ title, desc }: FeatureItem) => {
+    return (
+        <li className="flex w-full flex-1 flex-col items-start">
+            <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
+                {title}
+            </h2>
+            <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+                {desc}
+            </p>
+        </li>
+    )
+}
 
 const TermsOfUse = async ({
     params: { lang },
@@ -10,11 +25,22 @@ const TermsOfUse = async ({
     params: { lang: Locale }
 }) => {
 
+    const { document } = await geMessages(lang)
+    const valuea = document.filter((i) => i.names == "termsOfUse")[0];
+
     return (
         <>
-            {/* <Guide params={{ lang, names }} />
-            <Features params={{ lang, names }} />
-            <GetApp params={{ lang, names }} /> */}
+            <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-10">
+                <div className="max-container padding-container relative w-full flex justify-end">
+                    <div className="z-20 flex w-full flex-col lg:w-[60%]">
+                        <ul className="mt-0 grid gap-10 md:grid-cols-1 lg:mg-20 lg:gap-20">
+                            <FeatureItem
+                                title={valuea.title}
+                                desc={valuea.detai} />
+                        </ul>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
