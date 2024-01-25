5f2239dbaf561f52ba3e330fb9bac3cfef4b136f
mehmetsaitdas <mehmetsaitdas@hotmail.com>
2024-01-25 17:52:53 +0300

    vue_get_started

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <h1>{{ $t("home.header") }}</h1>
+    <p>{{ $t("home.created_by", {company: "Lokalise"}) }}</p>
+    <p>{{ $t("home.num_visits", 1) }}</p>
+    <p>{{ $d(new Date(), "longFormat") }}</p>
+
+    <li v-for="item in items">
+      {{ item.message }}
+    </li>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          message: 'Foo'
+        },
+        {
+          message: 'Bar'
+        }
+      ]
+    }
+  }
+}
+</script>
